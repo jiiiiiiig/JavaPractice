30ce7fac846588cf2266647d7afcefe2b08b10a5
박지교 <@JG>
2017-05-12 14:36:06 +0900

    실습과제 11

---
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -0,0 +1,29 @@
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *  org.dimigo.inheritance
+ *  └IPhone
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 12.
+ * </pre>
+ * 
+ * @author : 박지교
+ * @version : 1.0
+ */
+public class IPhone extends SmartPhone{
+	public IPhone(){
+		
+	}
+	public IPhone(String model, String company, int price){
+		super(model, company, price);
+	}
+	
+	public void pay(){
+		System.out.println("애플 페이로 결제합니다.");
+	}
+	public void useAirDrop(){
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+}
